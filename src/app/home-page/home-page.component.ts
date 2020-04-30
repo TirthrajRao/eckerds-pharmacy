0e7f016c03e2ae76992047c085a7a866bba08dc2
komalsakhiya <komalsakhiya21@gmail.com>
2020-04-30 16:08:51 +0530

    changes in home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router:Router
+  ) { }
 
   ngOnInit() {
   
+  }
+
+  getLocation(data){
+    let navigationExtras: NavigationExtras = {
+      state: data
+    };
+    this.router.navigate(['/home/location'], navigationExtras);
   }
 
 }
